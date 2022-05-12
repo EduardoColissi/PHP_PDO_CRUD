49a2ca8fa3a3aed7d9aafbc6db2a77ec9bb058b5
EduardoColissi <educolissi@hotmail.com>
2022-05-12 10:07:49 -0300

    :sparkles: feat: added prepared statements

--- index.php
+++ index.php
@@ -15,7 +15,25 @@
     $conn = new Conn();
     $conn->getConn();
 
-    var_dump($conn);
+    $email = "eduardo@gmail.com";
+    $usuario = "eduardo";
+    $senha = "123";
+
+    try {
+        $result_cadastrar = "INSERT INTO usuarios (email, usuario, senha, created) VALUES (:email, :usuario, :senha, NOW())";
+        $cadastrar = $conn->getConn()->prepare($result_cadastrar);
+
+        $cadastrar->bindParam(':email', $email, PDO::PARAM_STR);
+        $cadastrar->bindParam(':usuario', $usuario, PDO::PARAM_STR);
+        $cadastrar->bindParam(':senha', $senha, PDO::PARAM_STR);
+
+        $cadastrar->execute();
+
+        if ($cadastrar->rowCount()) :
+            echo "Cadastrado com sucesso!";
+        endif;
+    } catch (Exception $ex) {
+    }
     ?>
 </body>
 
