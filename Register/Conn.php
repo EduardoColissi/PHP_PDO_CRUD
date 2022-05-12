49ea24e214224698403fd8eff12fbdce36753523
EduardoColissi <educolissi@hotmail.com>
2022-05-12 10:44:54 -0300

    :sparkles: feat: added user registration

---
+++ Register/Conn.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conn
+{
+    public static $Host = "localhost";
+    public static $User = "root";
+    public static $Pass = "";
+    public static $Dbname = "pdo_crud";
+    private static $Connect = null;
+
+    private static function Conectar()
+    {
+        try {
+            if (self::$Connect == null) :
+                self::$Connect = new PDO('mysql:host=' . self::$Host . ';dbname=' . self::$Dbname, self::$User, self::$Pass);
+            endif;
+        } catch (Exception $ex) {
+            echo 'Mensagem' . $ex->getMessage();
+            die;
+        }
+        return self::$Connect;
+    }
+
+    public function getConn()
+    {
+        return self::Conectar();
+    }
+}
